06d8d67cfc8b6ba3c1ac7ba86db54d1c55a955b0
backmeupplz <backmeupplz@gmail.com>
2019-11-05 07:48:58 -0800

    update

--- src/assets/localization.ts
+++ src/assets/localization.ts
@@ -1,10 +1,52 @@
 export const messages = {
   ru: {
-    title: 'Минтер — скам',
+    title: 'Ставки на Минтер',
     notFound: 'Страница не найдена',
+    cookie: {
+      button: 'Отличненько!',
+      message:
+        'Мы за вами не следим. Мы используем куки только для того, чтобы запомнить ваши локальные настройки в роде темной темы и языка.',
+    },
+    home: {
+      info: 'Заработай на мошенничестве Минтера!',
+      premises: [
+        'Premise is simple: we have two Ethereum wallets on this website — one to bet on Minter not being a scam, one to bet on Minter being a scam. Participants send money to one of the addresses below to bet on whether BIP price is going to be above (not scam) or below (scam) the ICO price of $0.06 for 1 BIP by January 10th. 2021.',
+        'On January 10th, 2021, administrators of <a href="https://t.me/mcafee_approves">Avocado Approves</a> community backed by <a href="https://t.me/golden_borodutch">Golden Borodutch</a> will evaluate the buy price of BIP using <a href="https://monsternode.net">monsternode.net</a>. In the case when <a href="https://monsternode.net">monsternode.net</a> is not accessible, administrators have the right to evaluate price of BIP on any open market with the publicly available list of orders and completed deals.',
+        "Whoever wins, takes back their bet and a portion of loosers' bets proportional to the winner's bet. In case if there is no way to identify whether Minter became a scam or not by January 10th, 2021, all betters receive their bets back, minus Ethereum transaction fees.",
+        'This website takes no fees, no cuts, it\'s all for free. We host this website on GitHub Pages for free, hold Ethereum on the wallets for free, it is simply unfair to charge the participants. If you have any further questions, feel free to reach out to the administration of <a href="https://t.me/mcafee_approves">Avocado Approves</a> community.',
+        'It is time to bet: no matter what you believe in, you can now earn money if your belief is correct. Participate today, send Ethereum to the following addresses. Capitalize on your decision.',
+      ],
+    },
+    scam: 'Minter is a scam',
+    notScam: 'Minter is not a scam',
+    scamBet: 'People have sent {amount} ETH betting that Minter is a scam',
+    notScamBet:
+      'People have sent {amount} ETH betting that Minter is not a scam',
+    opensource: 'This website is open-source.',
   },
   en: {
-    title: 'Minter — scam',
+    title: 'Minter Bets',
     notFound: 'Page not found',
+    cookie: {
+      button: 'Alrighty!',
+      message:
+        'We do not track you. We use cookies only to remember your local settings like dark or light mode and language.',
+    },
+    home: {
+      info: 'Earn money on the Minter scam scheme!',
+      premises: [
+        'Premise is simple: we have two Ethereum wallets on this website — one to bet on Minter not being a scam, one to bet on Minter being a scam. Participants send money to one of the addresses below to bet on whether BIP price is going to be above (not scam) or below (scam) the ICO price of $0.06 for 1 BIP by January 10th. 2021.',
+        'On January 10th, 2021, administrators of <a href="https://t.me/mcafee_approves">Avocado Approves</a> community backed by <a href="https://t.me/golden_borodutch">Golden Borodutch</a> will evaluate the buy price of BIP using <a href="https://monsternode.net">monsternode.net</a>. In the case when <a href="https://monsternode.net">monsternode.net</a> is not accessible, administrators have the right to evaluate price of BIP on any open market with the publicly available list of orders and completed deals.',
+        "Whoever wins, takes back their bet and a portion of loosers' bets proportional to the winner's bet. In case if there is no way to identify whether Minter became a scam or not by January 10th, 2021, all betters receive their bets back, minus Ethereum transaction fees.",
+        'This website takes no fees, no cuts, it\'s all for free. We host this website on GitHub Pages for free, hold Ethereum on the wallets for free, it is simply unfair to charge the participants. If you have any further questions, feel free to reach out to the administration of <a href="https://t.me/mcafee_approves">Avocado Approves</a> community.',
+        'It is time to bet: no matter what you believe in, you can now earn money if your belief is correct. Participate today, send Ethereum to the following addresses. Capitalize on your decision.',
+      ],
+    },
+    scam: 'Minter is a scam',
+    notScam: 'Minter is not a scam',
+    scamBet: 'People have sent {amount} ETH betting that Minter is a scam',
+    notScamBet:
+      'People have sent {amount} ETH betting that Minter is not a scam',
+    opensource: 'This website is open-source.',
   },
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,22 @@
 <template lang="pug">
-  .v-container.pa-4
+  .v-container.pa-4(style='maxWidth: 1000px; margin: auto')
     // Main content
     v-layout(column
     justify-center
     align-center).text-center
       v-flex(xs12 md10)
         .headline.pb-4
-          span(v-html='$t("home.info")')
-        p {{$t('home.rules.register')}}
-        p {{$t('home.rules.money')}}
-        p {{$t('home.rules.success')}}
+          span {{$t("home.info")}}
+        p(v-for='premise in $t("home.premises")' v-html='premise')
+        h2 {{$t("scam")}}
+        h3
+          a(href="https://etherscan.io/address/123") 123
+        p {{$t("scamBet", { amount: 50 })}}
+        h2 {{$t("notScam")}}
+        h3
+          a(href="https://etherscan.io/address/123") 123
+        p {{$t("notScamBet", { amount: 10 })}}
+        a.pt-4(href="https://github.com/backmeupplz/minterscam") {{$t("opensource")}}
 </template>
 
 <script lang="ts">
